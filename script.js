b3ae726d60fd1c1a66c946c95c899e847ec90340
Alessio Carletti <alessio.carletti@protonmail.com>
2024-08-03 16:55:28 +0200

    new version

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ class CreateParticles {
 
 		this.data = {
 
-			text: ' Discover the real power\nof Bitcoin',
+			text: 'ENTER\nNOW',
 			amount: 1500,
 			particleSize: 1,
 			particleColor: 0xffffff,
